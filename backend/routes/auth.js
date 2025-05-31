6ca289a3d337fe22d701aeee20b84ec9d1736607
Shirshendu R Tripathi <shirsh8924@gmail.com>
2025-05-31 19:57:37 +0530

    test

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,7 +8,6 @@ router.get('/google', passport.authenticate('google', {
 router.get('/google/callback', 
     passport.authenticate('google', { failureRedirect: '/login' }),
     (req, res) => {
-        req.session.passport.user = req.user;
         res.redirect(process.env.CLIENT_URL+ '/#/dashboard');
     }
 );
